8af2955f1b9056d51a7abd521ee1155e42835580
JioChoi <jiochoi7@gmail.com>
2024-05-27 16:40:30 +0900

    Change max comments

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ async function generateArticle(url) {
 		console.log(title);
 
 		// Generate fake comments
-		let comments = Math.floor(Math.random() * 6);
+		let comments = Math.floor(Math.random() * 10);
 		for (let i = 0; i < comments; i++) {
 			let waitTime = Math.floor(Math.random() * 1000 * 60 * 3);
 
