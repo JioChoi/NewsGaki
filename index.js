037d8c679a3db727d6338733856c3f3b241ab35e
jiochoi <jiochoi7@gmail.com>
2025-01-17 21:51:55 -0500

    Change BLOCK_NONE to OFF

--- index.js
+++ index.js
@@ -824,19 +824,19 @@ async function gemini(prompt, retry = 0) {
 		const safetySettings = [
 			{
 				category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-				threshold: HarmBlockThreshold.BLOCK_NONE,
+				threshold: "OFF",
 			},
 			{
 				category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-				threshold: HarmBlockThreshold.BLOCK_NONE,
+				threshold: "OFF",
 			},
 			{
 				category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-				threshold: HarmBlockThreshold.BLOCK_NONE,
+				threshold: "OFF",
 			},
 			{
 				category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-				threshold: HarmBlockThreshold.BLOCK_NONE,
+				threshold: "OFF",
 			}
 		];
 
